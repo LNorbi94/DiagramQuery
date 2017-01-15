2c03442c4a6b6f82d37eb4dfdba04c17290f91b7
Norbert Lestár <lestar.norbert@gmail.com>
2017-01-15 15:25:56 +0100

    VS project

--- connectwindow.cpp
+++ connectwindow.cpp
@@ -1,14 +1,155 @@
 #include "connectwindow.h"
 #include "ui_connectwindow.h"
+#include "constants.h"
+
+#include <iostream>
+#include <fstream>
+#include <QXmlStreamWriter>
+#include <QDir>
+
+#include <QSqlDatabase>
+#include <QSqlError>
+
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QListWidgetItem>
 
 ConnectWindow::ConnectWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::ConnectWindow)
+    QMainWindow(parent)
+    , ui(new Ui::ConnectWindow)
+    , i(0)
 {
     ui->setupUi(this);
+	loadConnections();
 }
 
 ConnectWindow::~ConnectWindow()
 {
     delete ui;
 }
+
+void ConnectWindow::loadConnections()
+{
+	ui->lwConnections->clear();
+	QDir dir(connections::CONFIGFOLDER);
+	if (dir.exists())
+	{
+		QStringList filters;
+		filters << "*.xml";
+		dir.setNameFilters(filters);
+
+		QFileInfoList list = dir.entryInfoList();
+		for (int i = 0; i < list.size(); ++i)
+		{
+			QString name = list.at(i).baseName();
+			ui->lwConnections->addItem(name);
+		}
+	}
+}
+
+
+void ConnectWindow::on_pbConnect_clicked()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QOCI");
+    db.setHostName("eszakigrid97@aramis.inf.elte.hu");
+    db.setPort(1521);
+    //db.setDatabaseName("customdb");
+    db.setUserName("A8UZ7T");
+    db.setPassword("fTzg35!");
+    bool ok = db.open();
+    if (ok)
+    {
+        QMessageBox popup;
+        popup.setText("OK");
+        popup.exec();
+    } else {
+        QMessageBox popup;
+        popup.setText(db.lastError().text());
+        popup.exec();
+    }
+}
+
+void ConnectWindow::on_lwConnections_itemClicked(QListWidgetItem *item)
+{
+	QString filename = connections::CONFIGFOLDER;
+	filename.append("\\");
+	filename.append(item->text());
+	filename.append(".xml");
+	QFile file(filename);
+	if (file.open(QIODevice::ReadOnly))
+	{
+		QXmlStreamReader xmlReader;
+		xmlReader.setDevice(&file);
+		while (!xmlReader.atEnd())
+		{
+			if (xmlReader.isStartElement())
+			{
+				QStringRef tagname = xmlReader.name();
+				if (tagname.compare("port") == 0)
+				{
+					xmlReader.readNext();
+					QString port = xmlReader.text().toString();
+					ui->tEditPort->setPlainText(port);
+				}
+				else if (tagname.compare("host") == 0)
+				{
+					xmlReader.readNext();
+					QString host = xmlReader.text().toString();
+					ui->tEditHost->setPlainText(host);
+				}
+				else if (tagname.compare("username") == 0)
+				{
+					xmlReader.readNext();
+					QString username = xmlReader.text().toString();
+					ui->tEditUsername->setPlainText(username);
+				}
+				else if (tagname.compare("service") == 0)
+				{
+					xmlReader.readNext();
+					QString service = xmlReader.text().toString();
+					ui->tEditService->setPlainText(service);
+				}
+			}
+			xmlReader.readNext();
+		}
+	}
+}
+
+void ConnectWindow::on_pbSave_clicked()
+{
+	QInputDialog input;
+	input.setCancelButtonText("Mégse");
+	input.setInputMode(QInputDialog::TextInput);
+	input.setOkButtonText("Mentés");
+	input.setWindowTitle("Kapcsolat mentése");
+	input.setLabelText("Kérem írja be milyen néven kívánja menteni a kapcsolatot!");
+	input.setBaseSize(QSize(200, 100));
+	if ( input.exec() )
+	{
+		if ( !QDir(connections::CONFIGFOLDER).exists() )
+		{
+			QDir().mkdir(connections::CONFIGFOLDER);
+		}
+		QString filename = connections::CONFIGFOLDER;
+		QString title = input.textValue();
+		filename.append("\\");
+		filename.append(title);
+		filename.append(".xml");
+		std::cout << filename.toStdString();
+		QFile file(filename);
+		file.open(QIODevice::WriteOnly);
+		QXmlStreamWriter stream(&file);
+		stream.setAutoFormatting(true);
+		stream.writeStartDocument();
+		stream.writeStartElement("Connection");
+		stream.writeAttribute("name", title);
+		stream.writeTextElement("host", ui->tEditHost->toPlainText());
+		stream.writeTextElement("port", ui->tEditPort->toPlainText());
+		stream.writeTextElement("service", ui->tEditService->toPlainText());
+		stream.writeTextElement("username", ui->tEditUsername->toPlainText());
+		stream.writeEndElement();
+		stream.writeEndDocument();
+		loadConnections();
+	}
+}
+
--- connectwindow.h
+++ connectwindow.h
@@ -2,9 +2,11 @@
 #define CONNECTWINDOW_H
 
 #include <QMainWindow>
+#include <QListWidgetItem>
 
-namespace Ui {
-class ConnectWindow;
+namespace Ui
+{
+	class ConnectWindow;
 }
 
 class ConnectWindow : public QMainWindow
@@ -15,8 +17,18 @@ public:
     explicit ConnectWindow(QWidget *parent = 0);
     ~ConnectWindow();
 
+	void loadConnections();
+
+private slots:
+    void on_pbConnect_clicked();
+
+    void on_lwConnections_itemClicked(QListWidgetItem *item);
+
+	void on_pbSave_clicked();
+
 private:
     Ui::ConnectWindow *ui;
+    size_t i;
 };
 
 #endif // CONNECTWINDOW_H
