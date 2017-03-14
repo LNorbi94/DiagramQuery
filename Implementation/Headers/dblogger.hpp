e73e2bf45d30191a7a6186ef45f7c127f70bf17a
LNorbi <lestar.norbert@gmail.com>
2017-03-14 22:32:03 +0100

    Adding Headers..

---
+++ Implementation/Headers/dblogger.hpp
@@ -0,0 +1,28 @@
+﻿#ifndef DBLOGGER_HPP
+#define DBLOGGER_HPP
+
+#include <QObject>
+#include <QPlainTextEdit>
+#include <QProgressBar>
+
+#include <QTime>
+#include <QElapsedTimer>
+
+#include <functional>
+
+class DBLogger : public QPlainTextEdit
+{
+	Q_OBJECT
+
+public:
+    DBLogger(QWidget * parent = nullptr, QProgressBar* progressBar = nullptr);
+
+	void appendPlainText(const QString& text);
+	bool logWithTime(const QString & success, const QString& fail, std::function<bool()>& slowFunc);
+    void setProgressBar(QProgressBar* progressBar);
+
+private:
+    QProgressBar* progressBar;
+};
+
+#endif
---
+++ Implementation/Headers/sqlhighlighter.hpp
@@ -0,0 +1,34 @@
+﻿#ifndef SQLHIGHLIGHTER_HPP
+#define SQLHIGHLIGHTER_HPP
+
+#include <QObject>
+#include <QSyntaxHighlighter>
+
+class SqlHighlighter : public QSyntaxHighlighter
+{
+    Q_OBJECT
+
+public:
+    SqlHighlighter(QTextDocument * parent = nullptr);
+    ~SqlHighlighter();
+
+protected:
+    void highlightBlock(const QString &text) override;
+
+private:
+    struct Rule
+    {
+        QRegExp pattern;
+        QTextCharFormat format;
+    };
+    QVector<Rule> _rules;
+
+    QRegExp _commentExpr;
+
+    QTextCharFormat _keywordForm;
+    QTextCharFormat _functionForm;
+    QTextCharFormat _commentForm;
+    QTextCharFormat _literalForm;
+};
+
+#endif // SQLHIGHLIGHTER_HPP
